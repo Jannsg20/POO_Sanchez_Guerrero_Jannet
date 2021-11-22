c12e1ef395ef2daae0a9d46eead402ad682694e7
Jannsg20 <j.sanchez.g4@edu.utc.mx>
2021-11-21 20:18:41 -0600

    Arreglos y Cadenas

---
+++ 06_Figuras/Principal.java
@@ -0,0 +1,11 @@
+/*
+Ejemplos de figuras
+*/
+public class Principal{
+    public static void main(String[]args){
+        //metodo principal
+        //instancia de la clase
+        Figura objfigura=new Figura();
+        objfigura.menu();
+    }
+}
